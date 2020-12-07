3cf19d2807fb5c2327c7aa5071472cf05c4d92a2
jojacobsen <Jojo5589>
2020-12-07 11:20:55 +0100

    format aob Liste.

--- lib/widgets/aob_item.dart
+++ lib/widgets/aob_item.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
 
 import '../providers/aob.dart';
 import '../providers/aob_list.dart';
+
+final formatter =
+    new NumberFormat.simpleCurrency(locale: 'eu', decimalDigits: 0);
 
 class AobItem extends StatefulWidget {
   @override
@@ -60,11 +64,11 @@ class _AobItemState extends State<AobItem> {
             .map((aob) => DataRow(cells: [
                   DataCell(Text(aob.medium)),
                   DataCell(Text(aob.brand)),
-                  DataCell(Text(aob.goal.toString())),
-                  DataCell(Text(aob.offen.toString())),
-                  DataCell(Text(aob.gebucht.toString())),
-                  DataCell(
-                      Text((aob.goal - aob.offen - aob.gebucht).toString())),
+                  DataCell(Text(formatter.format(aob.goal))),
+                  DataCell(Text(formatter.format(aob.offen))),
+                  DataCell(Text(formatter.format(aob.gebucht))),
+                  DataCell(Text(
+                      formatter.format(aob.goal - aob.offen - aob.gebucht))),
                 ]))
             .toList(),
       ),
