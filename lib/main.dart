10f3f886c76370dd0273c210c8c0b4060dfa82cb
jojacobsen <Jojo5589>
2021-01-28 11:24:09 +0100

    change API Host to salescontrol.

--- lib/main.dart
+++ lib/main.dart
@@ -21,8 +21,8 @@ import './screens/project_forecast_screen.dart';
 import './screens/customer_forecast_screen.dart';
 
 String APIPROTOCOL = 'http://';
-String APIHOST = 'hammbwdsc02:96';
-//String APIHOST = 'salescontrolapi.visoon.de';
+//String APIHOST = 'hammbwdsc02:96';
+String APIHOST = 'salescontrolapi.visoon.de';
 
 void main() => runApp(VisoonApp());
 
